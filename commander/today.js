58ba642816b0cd39d77951dfce5d4f78f444c3df
jungheeeun <jungheeeun272@gmail.com>
2023-04-30 01:03:20 +0900

    moment 모듈과 format 메서드

--- commander/today.js
+++ commander/today.js
@@ -1,10 +1,10 @@
 const { program } = require('commander');
-const moment = require('moment');
+const moment = require('moment'); // 오늘 날짜 가져오기 
 
 program
   .version('1.0.0')
   .description('Displays today\'s date')
-  .parse(process.argv);
+  .parse(process.argv); // 프로그램 실행 시 전달된 인자들을 파싱할 목적
 
-const today = moment().format('YYYY-MM-DD');
+const today = moment().format('YYYY-MM-DD');  // 컴활때 한 것 같은 느낌? 
 console.log(today);
